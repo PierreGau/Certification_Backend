60a68fad0cfc07331380bfacfbe561677203365a
Unknown <p.gauthier09@gmail.com>
2022-12-02 10:46:35 +0100

    Init project
    création du projet

---
+++ Certification_Backend/src/test/java/com/kwiky/backend/CertificationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwiky.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
